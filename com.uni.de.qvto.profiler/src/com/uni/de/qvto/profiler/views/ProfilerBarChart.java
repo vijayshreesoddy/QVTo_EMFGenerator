ffd2aa590c995222b3bad591062b64243af4d3ff
vijayshreesoddy <40870815+vijayshreesoddy@users.noreply.github.com>
2024-04-25 10:06:12 +0200

    Update ProfilerBarChart.java

--- com.uni.de.qvto.profiler/src/com/uni/de/qvto/profiler/views/ProfilerBarChart.java
+++ com.uni.de.qvto.profiler/src/com/uni/de/qvto/profiler/views/ProfilerBarChart.java
@@ -22,7 +22,7 @@ public class ProfilerBarChart {
 
 	 public ProfilerBarChart(String appTitle) {
 
-		    // Create Dataset Variable
+		    // Create Dataset 
 		    CategoryDataset dataset = createDataset();
 		    
 		    //Create chart
